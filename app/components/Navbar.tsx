b584125310c22f0c54196f4450a69cc27ce6d149
stellaWangg <stellawang827@gmail.com>
2024-02-14 17:37:20 +0000

    change logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,12 @@
 "use client";
 import React, { useState } from "react";
 import { FaBars } from "react-icons/fa";
+import logo from "../logo.svg";
+import Image from "next/image";
 const navItems = [
-  { text: "Home", path: "/" },
   { text: "Projects", path: "#projects" },
   { text: "Skills", path: "#skills" },
+  //   { text: "About", path: "#about" },
   { text: "Contact", path: "#contact" },
 ];
 
@@ -16,9 +18,10 @@ const Navbar = () => {
   return (
     <nav className="align-element flex flex-col md:flex-row  items-center justify-between">
       <div className="flex items-center justify-between w-full md:w-auto py-4">
-        <h2 className="text-3xl font-bold">
-          <a href="/">✦tella</a>
-        </h2>
+        <a href="/">
+          <Image src={logo} alt={"logo"} />
+        </a>
+
         <div onClick={handleOpen} className="md:hidden cursor-pointer ">
           {open ? (
             <FaBars className="rotate-90" size={45} />
@@ -30,7 +33,7 @@ const Navbar = () => {
 
       <ul
         className={
-          `flex gap-x-10 flex-col md:flex-row gap-12 items-center w-full md:w-auto absolute md:static top-[70px] h-[calc(100vh-70px)] left-0 right-0 md:h-auto transition-all ease-in-out duration-500 ` +
+          `flex gap-x-10 flex-col pt-20 md:flex-row sm:pt-0 gap-12 items-center w-full md:w-auto absolute md:static top-[70px] h-[calc(100vh-70px)] left-0 right-0 md:h-auto transition-all ease-in-out duration-500 ` +
           (open
             ? "translate-x-0 bg-stone-300"
             : "left-[-100%] md:translate-x-0")
